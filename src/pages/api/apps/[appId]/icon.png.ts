f7bbb7181372fb34e376f86fddc1f5bc4a1f5920
Clayton Fidelis <clayton.fidelis@live.com>
2024-12-17 22:20:38 -0300

    fix logo

--- src/pages/api/apps/[appId]/icon.png.ts
+++ src/pages/api/apps/[appId]/icon.png.ts
@@ -1,15 +1,19 @@
+import { apps } from "@/lib/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import fetch from "node-fetch";
 
 export default async function handler(
-  _req: NextApiRequest,
+  req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  // const { appId } = req.query;
+  const { appId } = req.query;
+  const app = apps.find((app) => app.slug === appId);
 
   try {
     // Example: Fetch icon from a cloud bucket
-    const iconUrl = `https://i0.wp.com/elite.kiwify.com.br/wp-content/uploads/2023/09/logo.png?w=300&ssl=1`;
+    const iconUrl =
+      app?.logoUrl ??
+      `https://i0.wp.com/elite.kiwify.com.br/wp-content/uploads/2023/09/logo.png?w=300&ssl=1`;
     const response = await fetch(iconUrl);
 
     if (!response.body || !response.ok) {
